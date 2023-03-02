19d6f5d3f40c0a815abf9dd70d914f71718b53c3
Miguel-player-09 <121067934+Miguel-player-09@users.noreply.github.com>
2023-03-01 21:15:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir";
+ var last_name = "Kapoor";
+var fullName= first_name.concat (last_name)
+console.log(fullName) 
+var a = 10; 
+ var b = 5;
+var max = Math.max(a, b)
+console.log (max)
